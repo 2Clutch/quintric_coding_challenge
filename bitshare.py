992b9cb64690a081a0b4a7f90eab67ca0ab2f7ae
Maximillian Naza <maximillianaza@gmail.com>
2018-08-05 10:42:26 -0400

    Add command line argument parser

--- bitshare.py
+++ bitshare.py
@@ -1,6 +1,11 @@
+import argparse
 from bitshares.account import Account
 
-account = Account("quintric-trust")
+user = argparse.ArgumentParser()
+user.add_argument("-u", "--user", required=True, help="bitshare username")
+args = vars(user.parse_args())
+
+account = Account("{}" .format(args["user"]))
 
 for balance in account.balances:
     print("\n", balance, "\n")
